08808c736b5726153da7dd7246eba1bf5c71795a
Henry <henryfernandezv@gmail.com>
2015-11-08 15:59:43 -0500

    arreglo volley

--- app/src/main/java/com/inventarioweb/empresa/inventarioandroid/View/login.java
+++ app/src/main/java/com/inventarioweb/empresa/inventarioandroid/View/login.java
@@ -30,7 +30,7 @@ public class login extends AppCompatActivity implements View.OnClickListener {
     private String url;
     private RequestQueue fRequestQueue;
     Context context;
-    final ProgressDialog progressDialog = ProgressDialog.show(this,"Espere...","Sincronizando.");
+    //final ProgressDialog progressDialog = ProgressDialog.show(this,"Espere...","Sincronizando.");
     private EditText txtUsuario;
     private EditText txtpass;
     private Button boton;
@@ -52,26 +52,28 @@ public class login extends AppCompatActivity implements View.OnClickListener {
             }
         });*/
         context= this;
-
         url= getString(R.string.url_con);
-
         fRequestQueue= Volley.newRequestQueue(this);
-
-
         txtpass=(EditText)findViewById(R.id.txtPass);
-        String pass = txtpass.toString();
         txtUsuario= (EditText)findViewById(R.id.txtUsuario);
-        String email= txtUsuario.toString();
+        boton =(Button)findViewById(R.id.BtnAceptar);
+        boton.setOnClickListener((View.OnClickListener) this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String email= txtUsuario.getText().toString();
+        String pass = txtpass.getText().toString();
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
-                url+"/Usuario/validarLogin?email="+email+"&pass="+pass+"&tipo=1",
+                url+"/Usuario/validarUsuario?user="+email+"&pass="+pass,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
                             if(response.getBoolean("success")){
-                                //Intent principal = new Intent(Login.this,MenuMap.class);
-                                //startActivity(principal);
+                                Intent principal = new Intent(login.this,app.class);
+                                startActivity(principal);
                             }else {
                                 Toast.makeText(context, "Usuario No Valido", Toast.LENGTH_SHORT).show();
                             }
@@ -84,19 +86,11 @@ public class login extends AppCompatActivity implements View.OnClickListener {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.e("Volley", "ERROR");
+                        Log.e("Volley", "ERROR: "+error.getMessage());
                         Toast.makeText(context, "Error en el servidor", Toast.LENGTH_SHORT).show();
                     }
                 }
         );
         fRequestQueue.add(jsonObjectRequest);
-
-        boton =(Button)findViewById(R.id.BtnAceptar);
-        boton.setOnClickListener((View.OnClickListener) this);
-    }
-
-    @Override
-    public void onClick(View v) {
-
     }
 }
